791989f890801012c15068ec9324bff386b54840
Datadigicore <datadigicore@gmail.com>
2015-12-28 22:13:47 +0700

    Fix join datatable

--- application/libraries/L_datatable.php
+++ application/libraries/L_datatable.php
@@ -20,4 +20,16 @@ class l_datatable {
             SSP::simple( $_POST, $sql_details, $table, $primaryKey, $columns )
         );        
     }
+    function get_table_join_6($table1, $table2, $table3, $table4, $table5, $table6, $primaryKey, $columns, $where){
+        $sql_details = array(
+            'user' => $this->ci->db->username,
+            'pass' => $this->ci->db->password,
+            'db'   => $this->ci->db->database,
+            'host' => $this->ci->db->hostname
+        ); 
+        require( 'ssp.class.php' );
+        echo json_encode(
+            SSP::more_complex( $_POST, $sql_details, $table1, $table2, $table3, $table4, $table5, $table6, $primaryKey, $columns, $where)
+        );        
+    }
 }
