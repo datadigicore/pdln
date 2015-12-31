6cc8ab209914a6f022aeb4bf2eb625016ce1266d
kartikaDS <dhitatracker@gmail.com>
2015-12-31 15:03:29 +0700

    Revisi UI Tambah Permohonan

--- application/views/user/pdln-new-step2.php
+++ application/views/user/pdln-new-step2.php
@@ -14,75 +14,88 @@
             
             <form class="form-horizontal style-form" method="post" action="<?php echo base_url();?>home/process" enctype="multipart/form-data">
 
-               <?php if (isset($error_message)){?>
+              <?php if (isset($error_message)){?>
                 <input type="hidden" class="form-control" name="no_aplikasi" value="<?php echo $error_message['no_aplikasi_data_diri'];?>">
               <?php } ?> 
 
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label">No Surat Unit Utama</label>
-                  <div class="col-sm-9">
-                    <input type="text" class="form-control" name="no_surat_unit_utama" placeholder="No Surat Unit Utama">
+
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Surat Unit Utama</h3>
+                </div>
+                <div class="panel-body">
+                  <div class="form-group">
+                    <label class="col-lg-3 col-sm-3 control-label">No Surat Unit Utama</label>
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" name="no_surat_unit_utama" placeholder="No Surat Unit Utama">
+                    </div>
                   </div>
+
+                  <div class="form-group">
+                    <label class="col-lg-3 col-sm-3 control-label">Tanggal Surat Unit Utama</label>
+                    <div class="col-sm-9">
+                      <input type="date" class="form-control" name="tgl_surat_unit_utama" placeholder="Tanggal Surat Unit Utama">
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label class="col-lg-3 col-sm-3 control-label">Instansi Unit Utama</label>
+                    <div class="col-sm-9">
+                      <select class="form-control" id="instansi_unit_utama" name="instansi_unit_utama">
+                          <!-- query dari db -->
+                          <option value="-">---Pilih Instansi Unit Utama---</option>
+                          <?php foreach ($instansi as $key => $value) {
+                            echo '<option value="'.$value['id'].'">'.$value['nama_instansi'].'</option>';                          
+                          }?>
+                          <!-- <option value="Lainnya">Lain-lain</option> -->
+                      </select>
+
+                       <select class="form-control" id="sub_instansi_unit_utama" name="sub_instansi_unit_utama" >
+                          <!-- query dari db -->
+                          <option value="-">---Pilih Sub Instansi Unit Utama---</option>
+                          <!-- <option value="lainnya">Lain-lain</option> -->
+                      </select>                    
+
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label class="col-lg-3 col-sm-3 control-label">Pejabat Penandatangan Surat</label>
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" name="penandatangan_surat_unit_utama" placeholder="Pejabat Penandatangan Surat">
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label class="col-lg-3 col-sm-3 control-label">Perihal Surat</label>
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" name="perihal_surat_unit_utama" placeholder="Perihal Surat">
+                    </div>
+                  </div>
+                </div>
               </div>
 
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label">Tanggal Surat Unit Utama</label>
-                  <div class="col-sm-9">
-                    <input type="date" class="form-control" name="tgl_surat_unit_utama" placeholder="Tanggal Surat Unit Utama">
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Dokumen Pendukung</h3>
+                </div>
+                <div class="panel-body">
+                  <div class="form-group">
+                    <label class="col-lg-3 col-sm-3 control-label">Surat Unit Utama</label>
+                    <div class="col-sm-9">
+                      <input type="file" name="upl_files1" class="form-control">
+                    </div>
                   </div>
-              </div>
-
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label">Instansi Unit Utama</label>
-                  <div class="col-sm-9">
-                    <select class="form-control" id="instansi_unit_utama" name="instansi_unit_utama">
-                        <!-- query dari db -->
-                        <option value="-">---Pilih Instansi Unit Utama---</option>
-                        <?php foreach ($instansi as $key => $value) {
-                          echo '<option value="'.$value['id'].'">'.$value['nama_instansi'].'</option>';                          
-                        }?>
-                        <!-- <option value="Lainnya">Lain-lain</option> -->
-                    </select>
-
-                     <select class="form-control" id="sub_instansi_unit_utama" name="sub_instansi_unit_utama" >
-                        <!-- query dari db -->
-                        <option value="-">---Pilih Sub Instansi Unit Utama---</option>
-                        <!-- <option value="lainnya">Lain-lain</option> -->
-                    </select>                    
-
-                  </div>
-              </div>
-
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label">Pejabat Penandatangan Surat</label>
-                  <div class="col-sm-9">
-                    <input type="text" class="form-control" name="penandatangan_surat_unit_utama" placeholder="Pejabat Penandatangan Surat">
-                  </div>
-              </div>
-
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label">Perihal Surat</label>
-                  <div class="col-sm-9">
-                    <input type="text" class="form-control" name="perihal_surat_unit_utama" placeholder="Perihal Surat">
-                  </div>
-              </div>
-
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label">Surat Unit Utama</label>
-                  <div class="col-sm-9">
-                    <input type="file" name="upl_files1" class="form-control" style="width: auto;">
-                  </div>
+                </div>
               </div>
 
 
               <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label"></label>
-                  <div class="col-sm-9">
-                    <!--<button type="submit" class="btn pdln-btn">Lanjut</button>-->
-                    <input type="hidden" name="manage" value="add_surat_unit_utama">
-                    <a class="btn pdln-btn" title="Lanjut" onclick="$(this).closest('form').submit()"> Lanjut</a>
-                    <!-- <a href="<?php echo $base_url;?>pdln-view.php" class="btn pdln-btn">Selesai</a> -->
-                  </div>
+                <div class="col-sm-9">
+                  <input type="hidden" name="manage" value="add_surat_unit_utama">
+                  <a class="btn btn-primary" title="Lanjut" onclick="$(this).closest('form').submit()"><i class="fa fa-check-square-o"></i> Simpan dan Lanjut</a>
+                  <!-- <a href="<?php echo $base_url;?>pdln-view.php" class="btn pdln-btn">Selesai</a> -->
+                </div>
               </div>
 
             </form>
--- application/views/user/pdln-new-step3.php
+++ application/views/user/pdln-new-step3.php
@@ -14,119 +14,140 @@
             
             <form class="form-horizontal style-form" method="post" action="<?php echo base_url();?>home/process" enctype="multipart/form-data">
             
-               <?php if (isset($error_message)){?>
+              <?php if (isset($error_message)){?>
                 <input type="hidden" class="form-control" name="no_aplikasi" value="<?php echo $error_message;?>">
               <?php } ?> 
 
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label">No Surat Undangan</label>
-                  <div class="col-sm-9">
-                    <input type="text" class="form-control" name="no_surat_undangan" placeholder="No Surat">
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Surat Undangan Kunjungan</h3>
+                </div>
+                <div class="panel-body">
+                  <div class="form-group">
+                    <label class="col-lg-3 col-sm-3 control-label">No Surat Undangan</label>
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" name="no_surat_undangan" placeholder="No Surat">
+                    </div>
                   </div>
+
+                  <div class="form-group">
+                    <label class="col-lg-3 col-sm-3 control-label">Tanggal Surat Undangan</label>
+                    <div class="col-sm-9">
+                      <input type="date" class="form-control" name="tgl_surat_undangan" placeholder="Tanggal Surat">
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label class="col-lg-3 col-sm-3 control-label">Instansi Pengundang</label>
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" name="instansi_pengundang" placeholder="Instansi Unit Utama">
+                    </div>
+                  </div>
+                </div>
+              </div>
+                
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Informasi Kegiatan</h3>
+                </div>
+                <div class="panel-body">
+                  <div class="form-group">
+                    <label class="col-lg-3 col-sm-3 control-label">Negara Tujuan</label>                  
+                    <div class="col-sm-9">
+                      <select class="form-control" id="negara_tujuan" name="negara_tujuan">
+                          <!-- query dari db -->
+                          <option value="">---Pilih Instansi Unit Utama---</option>
+                          <?php foreach ($countries as $key => $value) {
+                            echo '<option value="'.$value['country_name'].'">'.$value['country_name'].'</option>';                          
+                          }?>
+                          <!-- <option value="Lainnya">Lain-lain</option> -->
+                      </select>                  
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label class="col-lg-3 col-sm-3 control-label">Waktu Kegiatan</label>
+                    <div class="col-sm-4">
+                      <input type="date" class="form-control" name="tgl_awal_kegiatan" placeholder="Waktu Mulai Kegiatan">
+                    </div>
+                    <div class="col-sm-1"><p class="text-center" style="margin: 5px 0;">s/d</p></div>
+                    <div class="col-sm-4">
+                      <input type="date" class="form-control" name="tgl_akhir_kegiatan" placeholder="Waktu Akhir Kegiatan">
+                    </div>
+                  </div> 
+
+                  <div class="form-group">
+                    <label class="col-lg-3 col-sm-3 control-label">Kategori Kegiatan</label>
+                    <div class="col-sm-9">
+                      <select class="form-control" name="kategori_kegiatan">
+                          <option value="Workshop">Workshop</option>
+                          <option value="Seminar">Seminar</option>
+                          <option value="Belajar">Belajar</option>
+                          <option value="Conferrence">Konferensi</option>
+                          <option value="Pertemuan">Pertemuan</option>
+                          <option value="Pelatihan">Pelatihan</option>
+                          <option value="Lainnya">Lainnya</option>
+                      </select>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label class="col-lg-3 col-sm-3 control-label">Rincian Kegiatan</label>
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" name="rincian_kegiatan" placeholder="Rincian_Kegiatan">
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label class="col-lg-3 col-sm-3 control-label">Sumber Dana</label>
+                    <div class="col-sm-9">
+                      <select class="form-control" name="sumber_dana_kegiatan">
+                          <option value="APBN">APBN (Pemerintah)</option>
+                          <option value="Pribadi">Pribadi (Dana Sendiri)</option>
+                          <option value="Pengundang">Pengundang (Sponsor)</option>
+                          <option value="lain-lain">Lain-lain (Seniman )</option>
+                      </select>
+                    </div>
+                  </div>
+
+                <div class="form-group">
+                    <label class="col-lg-3 col-sm-3 control-label">Keterangan Sumber Dana</label>
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" name="keterangan_sumber_dana_kegiatan" placeholder="Mis : JICA dan Kemendikbud">
+                    </div>
+                </div>
+                </div>
               </div>
 
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label">Tanggal Surat Undangan</label>
-                  <div class="col-sm-9">
-                    <input type="date" class="form-control" name="tgl_surat_undangan" placeholder="Tanggal Surat">
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Dokumen Pendukung</h3>
+                </div>
+                <div class="panel-body">
+                  <div class="form-group">
+                    <label class="col-lg-3 col-sm-3 control-label">Surat Undangan</label>
+                    <div class="col-sm-9">
+                      <input type="file" name="upl_files1" class="form-control">
+                    </div>
                   </div>
+
+                  <div class="form-group">
+                    <label class="col-lg-3 col-sm-3 control-label">Surat Perjanjian</label>
+                    <div class="col-sm-9">
+                      <input type="file" name="upl_files2" class="form-control">
+                    </div>
+                  </div>
+                </div>
               </div>
 
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label">Instansi Pengundang</label>
-                  <div class="col-sm-9">
-                    <input type="text" class="form-control" name="instansi_pengundang" placeholder="Instansi Unit Utama">
-                  </div>
-              </div>
-
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label">Negara Tujuan</label>                  
-                  <div class="col-sm-9">
-                    <select class="form-control" id="negara_tujuan" name="negara_tujuan">
-                        <!-- query dari db -->
-                        <option value="">---Pilih Instansi Unit Utama---</option>
-                        <?php foreach ($countries as $key => $value) {
-                          echo '<option value="'.$value['country_name'].'">'.$value['country_name'].'</option>';                          
-                        }?>
-                        <!-- <option value="Lainnya">Lain-lain</option> -->
-                    </select>                  
-                  </div>
-              </div>
-
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label">Waktu Kegiatan</label>
-                  <div class="col-sm-4">
-                    <input type="date" class="form-control" name="tgl_awal_kegiatan" placeholder="Waktu Mulai Kegiatan">
-                  </div>
-                  <div class="col-sm-4">
-                    <input type="date" class="form-control" name="tgl_akhir_kegiatan" placeholder="Waktu Akhir Kegiatan">
-                  </div>
-              </div> 
-
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label">Kategori Kegiatan</label>
-                  <div class="col-sm-9">
-                    <select class="form-control" name="kategori_kegiatan">
-                        <option value="Workshop">Workshop</option>
-                        <option value="Seminar">Seminar</option>
-                        <option value="Belajar">Belajar</option>
-                        <option value="Conferrence">Confference</option>
-                        <option value="Pertemuan">Pertemuan</option>
-                        <option value="Pelatihan">Pelatihan</option>
-                        <option value="Lainnya">Lainnya</option>
-                    </select>
-                  </div>
-              </div>
-
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label">Rincian Kegiatan</label>
-                  <div class="col-sm-9">
-                    <input type="text" class="form-control" name="rincian_kegiatan" placeholder="Rincian_Kegiatan">
-                  </div>
-              </div>
-
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label">Sumber Dana</label>
-                  <div class="col-sm-9">
-                    <select class="form-control" name="sumber_dana_kegiatan">
-                        <option value="APBN">APBN (Pemerintah)</option>
-                        <option value="Pribadi">Pribadi (Dana Sendiri)</option>
-                        <option value="Pengundang">Pengundang (Sponsor)</option>
-                        <option value="lain-lain">Lain-lain (Seniman )</option>
-                    </select>
-                  </div>
-              </div>
-
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label">Keterangan Sumber Dana</label>
-                  <div class="col-sm-9">
-                    <input type="text" class="form-control" name="keterangan_sumber_dana_kegiatan" placeholder="Mis : JICA dan Kemendikbud">
-                  </div>
-              </div>
-
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label">Surat Undangan</label>
-                  <div class="col-sm-9">
-                    <input type="file" name="upl_files1" class="form-control" style="width: auto;">
-                  </div>
-              </div>
-
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label">Surat Perjanjian</label>
-                  <div class="col-sm-9">
-                    <input type="file" name="upl_files2" class="form-control" style="width: auto;">
-                  </div>
-              </div>
-
-              <div class="form-group">
-                  <label class="col-lg-3 col-sm-3 control-label"></label>
+                <div class="form-group">
                   <div class="col-sm-9">
                     <!--<button type="submit" class="btn pdln-btn">Lanjut</button>-->
                     <input type="hidden" name="manage" value="add_surat_undangan">
-                    <a class="btn pdln-btn" title="Selesai" onclick="$(this).closest('form').submit()"> Selesai</a>
+                    <a class="btn btn-primary" title="Selesai" onclick="$(this).closest('form').submit()"><i class="fa fa-check-square-o"></i> Selesai</a>
                     <!-- <a href="<?php echo $base_url;?>pdln-view.php" class="btn pdln-btn">Selesai</a> -->
                   </div>
-              </div>
+                </div>
 
             </form>
           </div>
