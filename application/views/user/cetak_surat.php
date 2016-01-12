861223099844489593a85b78c06efc55c3cd84c3
Datadigicore <datadigicore@gmail.com>
2016-01-12 15:00:38 +0700

    Fix Bug Cetak Setneg

--- application/views/user/cetak_surat.php
+++ application/views/user/cetak_surat.php
@@ -9,6 +9,7 @@
               <div class="form-group">
                   <input type="hidden" name="content" value="cetak_surat"> 
                   <input type="hidden" name="kondisi" value="cari">
+        
                   <label class="col-lg-3 col-sm-3 control-label">No Surat Unit Utama</label>
                   <div class="col-sm-8">
                     <input type="text" class="form-control" name="no_surat_bpkln_setneg" placeholder="No Surat Unit Utama ke Setneg">
@@ -24,6 +25,10 @@
             <?php if(count(array_filter($query))!=0){?>            
             <div class="text-center">
               <form target="blank" method="POST" action="<?php echo base_url();?>c_mpdf">
+                <input type="hidden" name="hasilsearch" value="<?php 
+                if (isset($query[0]['no_surat_bpkln_setneg'])) {
+                  echo $query[0]['no_surat_bpkln_setneg'];
+                }else{}?>">
                 <input type="hidden" name="banyak" value="<?php echo count(array_filter($query)); ?>">
                 <input type="hidden" name="no_aplikasi" value="<?php echo $query[0]['no_aplikasi_data_diri']; ?>">
 
